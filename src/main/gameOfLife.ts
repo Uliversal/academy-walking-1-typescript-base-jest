6528bd797a9101ce17a199adbb5861dac793525f
uligruener-cn <uli.gruener@cluno.com>
2021-09-07 11:35:37 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/gameOfLife.ts
+++ src/main/gameOfLife.ts
@@ -1,31 +1,50 @@
 import { Position } from "./Position";
 
 export class GameOfLife {
-
   public cells: Position[];
 
   public constructor() {
     this.cells = [];
   }
 
-  addCell(position: Position){
+  addCell(position: Position) {
     this.cells.push(position);
   }
 
-  printOutput (width: number, height: number) {
+  printOutput(width: number, height: number) {
     let output: string[][] = [];
     for (let x = 0; x < height; x++) {
-        output.push(new Array(width).fill('0'));
+      output.push(new Array(width).fill("0"));
     }
     for (const cell of this.cells) {
       if (cell.x < width && cell.y < height) {
-        output[cell.y][cell.x] = 'X';
+        output[cell.y][cell.x] = "X";
       }
-    }    
-    return output.map(e => e.join('')).join('\n');
+    }
+    const o = output.map((e) => e.join("")).join("\n");
+    console.log(o);
+    return o;
   }
 
   nextStep() {
-    
+    const newCells: Position[] = [];
+
+    for (const cell of this.cells) {
+      let numberOfNeighbours = -1;
+      for (const neighbour of this.cells) {
+        if (
+          neighbour.x >= cell.x - 1 &&
+          neighbour.x <= cell.x + 1 &&
+          neighbour.y >= cell.y - 1 &&
+          neighbour.y <= cell.y + 1
+        ) {
+          numberOfNeighbours++;
+        }
+      }
+      if (numberOfNeighbours === 2 || numberOfNeighbours === 3) {
+        newCells.push(cell);
+      }
+    }
+    this.cells = newCells;
   }
 }
--- src/test/gameOfLife.test.ts
+++ src/test/gameOfLife.test.ts
@@ -1,21 +1,40 @@
-import { GameOfLife } from './../main/gameOfLife';
+import { GameOfLife } from "./../main/gameOfLife";
 import { Position } from "../main/Position";
 
 describe("GameOfLife test", () => {
-
   it("should return 2x2 grid with two cells", () => {
     const gameOfLife = new GameOfLife();
-    gameOfLife.addCell({x:0,y:0});
-    gameOfLife.addCell({x:1,y:0});
-    expect(gameOfLife.printOutput(2,2)).toBe('XX\n00');
+    gameOfLife.addCell({ x: 0, y: 0 });
+    gameOfLife.addCell({ x: 1, y: 0 });
+    expect(gameOfLife.printOutput(2, 2)).toBe("XX\n00");
   });
 
-  it("should return 2x2 grid after one step", () => {
+  it("should return an empty 2x2 grid after one step because two cells died", () => {
     const gameOfLife = new GameOfLife();
-    gameOfLife.addCell({x:0,y:0});
-    gameOfLife.addCell({x:1,y:0});
+    gameOfLife.addCell({ x: 0, y: 0 });
+    gameOfLife.addCell({ x: 1, y: 0 });
     gameOfLife.nextStep();
-    expect(gameOfLife.printOutput(2,2)).toBe('00\n00');
+    expect(gameOfLife.printOutput(2, 2)).toBe("00\n00");
   });
 
+  it("should return a 4x4 grid after one step filled with cells", () => {
+    const gameOfLife = new GameOfLife();
+    gameOfLife.addCell({ x: 1, y: 1 });
+    gameOfLife.addCell({ x: 2, y: 1 });
+    gameOfLife.addCell({ x: 1, y: 2 });
+    gameOfLife.addCell({ x: 2, y: 2 });
+    gameOfLife.nextStep();
+    expect(gameOfLife.printOutput(4, 4)).toBe("0000\n0XX0\n0XX0\n0000");
+    gameOfLife.nextStep();
+    expect(gameOfLife.printOutput(4, 4)).toBe("0000\n0XX0\n0XX0\n0000");
+  });
+
+  it("should return a 4x4 grid after one step filled with cells", () => {
+    const gameOfLife = new GameOfLife();
+    gameOfLife.addCell({ x: 1, y: 1 });
+    gameOfLife.addCell({ x: 2, y: 1 });
+    gameOfLife.addCell({ x: 1, y: 2 });
+    gameOfLife.nextStep();
+    expect(gameOfLife.printOutput(4, 4)).toBe("0000\n0XX0\n0XX0\n0000");
+  });
 });
